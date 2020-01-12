2942079c061495de5787c185c3176e2728535509
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-12 18:05:46 +0000

    test(*): Experiment with e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ForbiddenException } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+const headers = {
+  authorization: 
+}
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/boards (GET)', async () => {
+    const res = await request(app.getHttpServer()).get('/boards');
+
+    expect(res.status).toBe(403);
+    expect(res.unauthorized);
+    expect(res.body).toEqual({ error: 'Forbidden', message: 'Forbidden resource', statusCode: 403 });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
