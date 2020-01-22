556d56997027ef254cc4c1de952902e12018bd56
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-22 00:08:49 +0000

    feat(board): On board create, create empty config entity

--- src/board/service/board.service.ts
+++ src/board/service/board.service.ts
@@ -3,16 +3,20 @@ import { CrudService } from '../../shared/abstracts/crud-service/crud-service.ab
 import { IBoard } from '../model/board';
 import { MongooseModel } from '../../shared/database/mongoose/constants';
 import { Model } from 'mongoose';
-import { IParams } from 'src/config/interfaces/params.interface';
+import { IParams } from '../../config/interfaces/params.interface';
+import { BoardConfigService } from '../../board-config/services/board-config.service';
 
 @Injectable()
 export class BoardService extends CrudService<IBoard> {
-  constructor(@Inject(MongooseModel.BOARD) boardModel: Model<IBoard>) {
+  constructor(@Inject(MongooseModel.BOARD) boardModel: Model<IBoard>, private readonly _boardConfigService: BoardConfigService) {
     super(boardModel);
   }
 
   public async findOne(query: IParams, userId: string): Promise<IBoard> {
-    const doc = await this._findOne(query, userId).populate('columns.stories');
+    const doc = await await this._findOne(query, userId)
+      .populate('columns.stories')
+      .populate('config')
+      .exec();
     if (!doc) throw new NotFoundException('The item you are looking for cannot be found!');
 
     return doc;
@@ -23,6 +27,14 @@ export class BoardService extends CrudService<IBoard> {
     if (!doc) throw new NotFoundException('The item you are looking for cannot be found!');
 
     return doc;
+  }
+
+  public async createBoard(body: Partial<IBoard>, userId: string): Promise<IBoard> {
+    const board = this._create(body, userId);
+    const savedBoard = await this._save(board);
+    await this._boardConfigService.create({ board: savedBoard._id }, userId);
+
+    return savedBoard;
   }
 
   // TODO - add a function for the logic for adding the story to the board is moved to the API instead of the UI
