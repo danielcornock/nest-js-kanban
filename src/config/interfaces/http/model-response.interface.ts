d0ba9d8f321ddd9fa74e352b51288a0ede6667d4
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-05 21:45:58 +0000

    feat(model-response): Create new model interfaces

--- src/config/interfaces/http/model-response.interface.ts
+++ src/config/interfaces/http/model-response.interface.ts
@@ -1,5 +1,21 @@
-interface IModelInstance<T> {
+import { Document } from 'mongoose';
+
+export interface IModelResponse<T extends Document> {
+  data: IModelData<T>;
+  meta: IModelMeta;
+}
+
+export interface IModelData<T extends Document> {
   [key: string]: T;
 }
 
-export type IModelPromise<T> = Promise<IModelInstance<T>>;
+export interface IModelMeta {
+  links: IModelLinks;
+}
+
+export interface IModelLinks {
+  self: string;
+  [key: string]: string;
+}
+
+export type IModelPromise<T extends Document> = Promise<IModelResponse<T>>;
