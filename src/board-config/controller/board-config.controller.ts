68634c2fe68d9b1be35dbab9ba97b2af22d7b8f0
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-05 21:00:41 +0000

    chore(board-config): Update return type to deprecated

--- src/board-config/controller/board-config.controller.ts
+++ src/board-config/controller/board-config.controller.ts
@@ -4,7 +4,7 @@ import { BaseController } from '../../shared/abstracts/base-controller.abstract.
 import { IBoardConfig } from '../model/board-config';
 import { BoardConfigService } from '../services/board-config.service';
 import { IReq } from '../../config/interfaces/middleware-params.interface';
-import { IModelPromise } from '../../config/interfaces/http/model-response.interface';
+import { IModelPromiseDeprecated } from '../../config/interfaces/http/model-response-deprecated.interface';
 
 @Controller()
 @UseGuards(AuthGuard)
@@ -14,7 +14,7 @@ export class BoardConfigController extends BaseController<IBoardConfig, BoardCon
   }
 
   @Put(`/:${this._nativeId}`)
-  public async update(@Body() body: IBoardConfig, @Param(`${this._nativeId}`) _id: string, @Req() req: IReq): IModelPromise<IBoardConfig> {
+  public async update(@Body() body: IBoardConfig, @Param(`${this._nativeId}`) _id: string, @Req() req: IReq): IModelPromiseDeprecated<IBoardConfig> {
     const config = await this._nativeService.update(body, req.user._id, { _id }).catch(e => {
       throw e;
     });
