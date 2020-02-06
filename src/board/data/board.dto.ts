4ffff9a9f2e415aa64169088f19b49d8b8e06c79
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-06 22:25:54 +0000

    feat(board-dto): Update columns in board dto

--- src/board/data/board.dto.ts
+++ src/board/data/board.dto.ts
@@ -1,4 +1,5 @@
 import { IsString, IsArray, IsOptional, IsNotEmpty } from 'class-validator';
+import { IColumn } from '../model/board';
 
 export class BoardDTO {
   @IsString()
@@ -7,5 +8,5 @@ export class BoardDTO {
 
   @IsArray()
   @IsOptional()
-  public readonly columns: Array<string>;
+  public readonly columns: Array<IColumn>;
 }
