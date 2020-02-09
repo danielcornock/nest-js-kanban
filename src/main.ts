99068871b18987f7ae822db4fc8fe2143f5f3037
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-09 16:36:08 +0000

    Heroku: Add security measures to app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './shared/interceptors/response/response.interceptor';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.enableCors();
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100
+    })
+  );
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
