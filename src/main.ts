f76a4736c254d3ccd22deadb78879d424e8de064
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-09 15:33:00 +0000

    heroku: attempted fix for cors issues

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 import { ResponseInterceptor } from './shared/interceptors/response/response.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api/v1');
   app.useGlobalInterceptors(new ResponseInterceptor());
-  app.enableCors({ origin: true });
+  // app.enableCors({ origin: true });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
